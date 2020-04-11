303d466ad48a0fec2af993a2c9a6a9b6ca518180
JordiRhoden <jordigilberto@gmail.com>
2020-04-11 00:48:01 -0600

    0.1.0 Alpha release

---
+++ client/src/components/Player/Player.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import './Player.css';
+
+const TextContainer = () => {
+  return(
+    <iframe 
+      width="700" 
+      height="400" 
+      src="https://www.youtube.com/embed/Fp9LMsI6uJ8" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+      allowfullscreen>
+    </iframe>
+  )
+  }
+
+export default TextContainer;
